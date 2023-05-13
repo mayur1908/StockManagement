ed641e394dae25166411112043621c109999d1c4
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-13 10:34:22 +0530

    added the read json stock files

---
+++ StockManagement/ReadTheData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StockManagement
+{
+    public class ReadTheData
+    {
+        public StocksDetail Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StocksDetail>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ StockManagement/StocksDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    public class StocksDetail
+    {
+        public List<TypesOfStocks> typesOfStocks;
+    }
+    public class TypesOfStocks
+    {
+        public string name; 
+        public int nofshare;
+        public int price;
+    }
+}
